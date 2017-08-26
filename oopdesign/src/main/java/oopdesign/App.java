97b236a474936d68bcbae35849d94edf4ddb8dda
Rakshit Pithadia <rpithadia25@gmail.com>
2017-08-26 14:28:53 -0700

    Initial Commit

---
+++ oopdesign/src/main/java/oopdesign/App.java
@@ -0,0 +1,13 @@
+package oopdesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
